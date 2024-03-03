cf3b057f213d833d5aa65a0d06ed6ef9e16faf6e
root <root@DESKTOP-RU622I8>
2024-03-03 19:34:42 +0100

    add ascii art

--- Program.cs
+++ Program.cs
@@ -6,12 +6,70 @@ using Tamagotchi;
 
 Pet pet = new Pet("Pascal");
 DateTime currentTime = DateTime.Now;
+Timer timer = new Timer(TamagotchiEgg, null, 0, 2000);
 
-Timer timer = new Timer(GameOnGoing, null, 0, 2000);
+
+while(Console.ReadLine() != )
 
 Console.ReadLine();
 
-static void GameOnGoing(object state){
+static void TamagotchiEgg(object state){
+  Console.Clear();
+   System.Console.WriteLine(@"
+   
+            .-~-.  
+          .'     '.           
+         /         \          
+        :           ; 
+        |           |        
+        :           :             
+         \         /             
+          `.     .'          
+            `~~~`   ");
+}
+
+
+static void Tamagotchihatched(object state){
+  
+  Console.Clear();
+
+  System.Console.WriteLine(@" 
+         _..._
+       .'     '.
+      /  _   _  \
+      | (o)_(o) |
+       \(     ) / 
+       / '._.'  \
+      |\   /\/ \/|   ,_
+      ; \/`     '; , \_',
+       \        /
+        '.    .'    /`.
+          `~~` , /\ ``
+              .  `");
+
+}
+
+static void TamagotchiBaby(object state){
+  
+  Console.Clear();
+
+  System.Console.WriteLine(@"
+
+           _..._
+         .'     '.
+        /  _   _  \
+        | (o)_(o) |
+         \(     ) /
+         //'._.'\ \
+        //   .   \ \
+       ||   .     \ \
+       |\   :     / |
+       \ `) '   (`  /_
+     _)``^.____,.'^` (_
+     )     )'--'(     (
+      '---`      `---`");
+}
+static void TamagotchiAdult(object state){
 
     Console.Clear();
 
@@ -34,4 +92,5 @@ _)      \.___.,|     .'
 \____   )MMMMMP|   .'
      `-'       `--'
     ");
+
 }
